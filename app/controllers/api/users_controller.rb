5d04ab511764958acf03664be42579ac6cf5f2f8
mwdeshaw <mwdeshaw@gmail.com>
2019-06-29 22:04:09 -0700

    Create users api controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,33 @@
+class Api::UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.nil?
+            render json: ["Could not find user"], status: 400
+        elsif @user.update_attributes(user_params)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+    attr_reader :password
+
     validates :email, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :password, length: { minimum: 6}, allow_nil: true
 
     after_initialize :ensure_session_token
-    attr_reader :password
 
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
