486b0f836ede6102717ae4797abaa785f2a18cd5
mwdeshaw <mwdeshaw@gmail.com>
2019-07-01 22:19:15 -0700

    Implement redux framework to manage session errors

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -1,8 +1,8 @@
 import * as APIUtil from '../util/session_utils';
 
-
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 
 export const receiveCurrentUser = user => ({
@@ -14,15 +14,26 @@ export const logoutCurrentUser = () => ({
     type: LOGOUT_CURRENT_USER,
 });
 
+export const receiveErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+});
+
 
 export const createNewUser = user => dispatch => (
     APIUtil.postUser(user)
-        .then(user => dispatch(receiveCurrentUser(user)))
+        .then(user => (dispatch(receiveCurrentUser(user))
+        ), err => (
+            dispatch(receiveErrors(err.responseJSON))
+        ))
 );
 
 export const login = user => dispatch => (
     APIUtil.postSession(user)
-        .then(user => dispatch(receiveCurrentUser(user)))
+        .then(user => (dispatch(receiveCurrentUser(user))
+        ), err => (
+            dispatch(receiveErrors(err.responseJSON))
+        ))
 );
 
 export const logout = () => dispatch => (
