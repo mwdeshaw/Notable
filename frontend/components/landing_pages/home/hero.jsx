4f2ce5bf67fdfdcc323ccf8aa2ef216f5d836cb7
mwdeshaw <mwdeshaw@gmail.com>
2019-07-03 22:26:16 -0700

    Add a few extra touches to splash page

--- frontend/components/landing_pages/home/hero.jsx
+++ frontend/components/landing_pages/home/hero.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const Hero = () => {
+
+
     return (
         <div className='big-hero'>
             <div className='hero-img'>
@@ -9,8 +12,8 @@ const Hero = () => {
                         <li><h1>Your Notes.</h1></li>
                         <li><h1>Organized.</h1></li>
                         <li><h1 className='last-h'>Effortless</h1></li>
-                        <li><p>Take notes anywhere. Find information faster. Share ideas with anyone. Meeting notes, web pages, projects, to-do lists—with Evernote as your note taking app, nothing falls through the cracks.</p></li>
-                        <button className='hero-button'><Link to='/signup'>TRY IT NOW</Link></button>
+                        <li><p>Take notes anywhere. Find information faster. Share ideas with anyone. Meeting notes, web pages, projects, to-do lists—with Notable as your note taking app, nothing falls through the cracks.</p></li>
+                        <button className='hero-button'><Link to='/'>TRY IT NOW</Link></button>
                     </ul> 
                 <img className='comp' src={window.compURL}/>
             </div>
--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter, Link, Redirect} from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 const styles = {
     logoText: {
